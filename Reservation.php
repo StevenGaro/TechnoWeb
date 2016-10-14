c99cf68a4e389fb09d256f2936d5bc43570be644
StevenGaro <steven.garofalo1@gmail.com>
2016-10-14 11:13:11 +0200

    add Reservation.php

---
+++ Reservation.php
@@ -0,0 +1,28 @@
+<html>
+	<head>
+		<title>Labo2 : Réservation</title>
+	</head>
+	
+	<body>
+	
+	<h1>Reservation</h1>
+	
+	Le prix de la place est de 10 euros jusqu'à 12 ans et ensuite de 15 euros 
+	<br>Le prix de l'assurance annulation est de 20 euros quel que soit le nombre de voyageurs.
+	<br>
+	
+	<table  cellspacing="1" 
+	cellpadding="1"><tr><td>Destination&nbsp;&nbsp;</td><td><input type="text"  /></td></tr>
+	<br>
+	<tr><td>Nombre de places&nbsp;</td><td><input type="text"  /></td></tr>
+	<br>
+	<tr><td>Assurance annulation&nbsp;</td><td><input type="checkbox" value="valeur" />
+	</td></tr></table>
+	<br>
+	<input type="button" value="Etape suivante"/>&nbsp;<input type="button" value="Annuler la réservation"/>
+	
+	</body>
+</html>
+	
+	
+	

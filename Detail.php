debd8dbaaad8036b758bbad7caeb4edf2678a50b
StevenGaro <steven.garofalo1@gmail.com>
2016-10-14 11:20:06 +0200

    add Detail.php

---
+++ Detail.php
@@ -0,0 +1,23 @@
+<html>
+	<head>
+		<title>Labo2 : Detail</title>
+	</head>
+	
+	<body>
+	
+	<h1>Detail de la reservation</h1>
+
+	<table  cellspacing="1" cellpadding="1">
+	<tr><td>Nom</td> <td><input type="text"  /></td></tr>
+	<br>
+	<tr><td>Age</td> <td><input type="text"  /></td></tr>
+	</table>
+	<br>
+	
+	<input type="button" value="Etape suivante"/>&nbsp;
+	<input type="button" value="Retour a la page precedente"/>&nbsp;
+	<input type="button" value="Annuler la réservation"/>
+	
+	</body>
+</html>
+	
